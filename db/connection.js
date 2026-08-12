87ede0fed94db379ec2f616e68cf664fad460f51
dompatnaude <dompatnaude1@gmail.com>
2026-08-12 19:41:53 +0000

    Fix Supabase database pool saturation

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,10 @@ const pool = new Pool({
 connectionString: process.env.DATABASE_URL,
 ssl: {
 rejectUnauthorized: false
-}
+},
+max: 8,
+idleTimeoutMillis: 30000,
+connectionTimeoutMillis: 10000
 });
 
 module.exports = pool;
